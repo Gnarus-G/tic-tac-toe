8fe86c89a926828e759c62bee9117aadcaf6ac6f
gnaru-g <g.avet.gnaritas@gmail.com>
2022-05-25 23:26:48 -0400

    feat: win condition checker

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+
 #[derive(Debug, PartialEq)]
 enum Move {
     X,
@@ -14,6 +15,57 @@ impl Board {
         Board {
             current: [[None, None, None], [None, None, None], [None, None, None]],
         }
+    }
+
+    pub fn check_lines(&mut self) -> Vec<Vec<(usize, usize)>> {
+        let row_indeces_iter = || 0..self.current.len();
+        let colum_indeces_iter = || 0..self.current[0].len();
+
+        let mut row_lines: Vec<Vec<(usize, usize)>> = self
+            .current
+            .iter()
+            .zip(row_indeces_iter())
+            .filter(|(row, _)| row.iter().any(|m| m.is_some())) //contains some move in row
+            .filter(|(row, _)| row.iter().all(|m| *m == row[0]))
+            .map(|(row, row_idx)| (0..row.len()).map(|col_idx| (row_idx, col_idx)).collect())
+            .collect();
+
+        let mut column_lines: Vec<Vec<(usize, usize)>> = (row_indeces_iter())
+            .map(|col_idx| {
+                ((colum_indeces_iter()).map(|i| &self.current[i][col_idx]))
+                    .collect::<Vec<&Option<Move>>>()
+            })
+            .zip(row_indeces_iter())
+            .filter(|(col, _)| col.iter().any(|&m| m.is_some())) //contains some move in column
+            .filter(|(col, _)| col.iter().all(|&m| *m == *col[0]))
+            .map(|(col, col_idx)| (0..col.len()).map(|row_idx| (row_idx, col_idx)).collect())
+            .collect();
+
+        row_lines.append(&mut column_lines);
+
+        //left diagonal?
+        if row_indeces_iter()
+            .map(|n| &self.current[n][n])
+            .all(|m| m.is_some() && *m == self.current[0][0])
+        {
+            row_lines.push(row_indeces_iter().map(|n| (n, n)).collect());
+        }
+
+        //right diagonal?
+        let right_diag_line_iter = || {
+            row_indeces_iter()
+                .rev()
+                .zip(colum_indeces_iter().inspect(|t| println!("c {:?}", t)))
+        };
+
+        if right_diag_line_iter()
+            .map(|(i, j)| &self.current[i][j])
+            .all(|m| m.is_some() && *m == self.current[0][2])
+        {
+            row_lines.push(right_diag_line_iter().collect());
+        }
+
+        row_lines
     }
 
     fn is_complete(&self) -> bool {
@@ -36,7 +88,16 @@ impl<'a> Player<'a> {
 
 #[cfg(test)]
 mod tests {
+    use std::vec;
+
     use crate::{Board, Move, Player};
+
+    fn x() -> Option<Move> {
+        Some(Move::X)
+    }
+    fn o() -> Option<Move> {
+        Some(Move::O)
+    }
 
     #[test]
     fn test_play_at_position() {
@@ -57,8 +118,6 @@ mod tests {
         let mut board = Board::new();
         assert!(!board.is_complete());
 
-        let x = || Some(Move::X);
-
         board.current = [[x(), x(), x()], [x(), x(), x()], [x(), x(), x()]];
 
         assert!(board.is_complete());
@@ -70,5 +129,58 @@ mod tests {
         board.current = [[x(), x(), x()], [x(), None, x()], [x(), x(), x()]];
 
         assert!(!board.is_complete());
+    }
+
+    #[test]
+    fn test_winning_lines() {
+        let mut board = Board::new();
+        let no_lines: Vec<Vec<(usize, usize)>> = vec![];
+        assert_eq!(board.check_lines(), no_lines);
+    }
+
+    #[test]
+    fn test_winning_rows() {
+        let mut board = Board::new();
+
+        board.current = [[x(), x(), x()], [o(), o(), x()], [o(), x(), o()]];
+        assert_eq!(board.check_lines(), vec![vec![(0, 0), (0, 1), (0, 2)]]);
+
+        board.current = [[o(), x(), o()], [o(), o(), x()], [x(), x(), x()]];
+        assert_eq!(board.check_lines(), vec![vec![(2, 0), (2, 1), (2, 2)]]);
+    }
+
+    #[test]
+    fn test_winning_lines_columns() {
+        let mut board = Board::new();
+
+        board.current = [[o(), x(), x()], [x(), x(), o()], [o(), x(), x()]];
+        assert_eq!(board.check_lines(), vec![vec![(0, 1), (1, 1), (2, 1)]]);
+
+        board.current = [[o(), x(), x()], [x(), x(), o()], [o(), x(), x()]];
+        assert_eq!(board.check_lines(), vec![vec![(0, 1), (1, 1), (2, 1)]]);
+    }
+    #[test]
+    fn test_winning_column_and_row() {
+        let mut board = Board::new();
+
+        board.current = [[o(), x(), x()], [o(), o(), x()], [x(), x(), x()]];
+        assert_eq!(
+            board.check_lines(),
+            vec![vec![(2, 0), (2, 1), (2, 2)], vec![(0, 2), (1, 2), (2, 2)]]
+        );
+    }
+    #[test]
+    fn test_winning_left_diagonal() {
+        let mut board = Board::new();
+
+        board.current = [[o(), x(), x()], [o(), o(), x()], [x(), o(), o()]];
+        assert_eq!(board.check_lines(), vec![vec![(0, 0), (1, 1), (2, 2)]]);
+    }
+
+    #[test]
+    fn test_winning_right_diagonal() {
+        let mut board = Board::new();
+        board.current = [[o(), x(), x()], [o(), x(), x()], [x(), o(), o()]];
+        assert_eq!(board.check_lines(), vec![vec![(2, 0), (1, 1), (0, 2)]]);
     }
 }
