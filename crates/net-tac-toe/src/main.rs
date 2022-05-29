bacf38ee08119b24024d74b1893e1e5c7b1d638a
gnaru-g <g.avet.gnaritas@gmail.com>
2022-05-29 15:44:31 -0400

    feat: proper argument parsing with clap, allowing for easy configuration

--- crates/net-tac-toe/src/main.rs
+++ crates/net-tac-toe/src/main.rs
@@ -1,42 +1,62 @@
+use clap::Parser;
 use std::{
-    env::args,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
 };
 use tic_tac_toe::game::GameInstance;
 use tic_tac_toe::{utils::input::Play, *};
 
-const PORT: u16 = 1234;
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+/// Play tic-tac-toe over TCP/IP with your friend(s).
+struct Args {
+    /// Play as X; which makes you the server of the p2p connection. X goes first.
+    #[clap(short)]
+    x: bool,
+
+    /// IP address of the X peer's connection. Required if you're not X.
+    #[clap(short, long, required_unless_present = "x")]
+    ip: Option<String>,
+
+    /// Port number of the X peer's connection.
+    #[clap(short = 'p', long, default_value = "1234")]
+    port: u16,
+}
 
 fn main() -> std::io::Result<()> {
-    if choose_be_server() {
+    let args = Args::parse();
+    let ip = &args.ip.unwrap_or("localhost".to_string());
+    let address = format!("{}:{}", ip, args.port);
+
+    if args.x {
         println!("awaiting a challenger...");
-        return as_server();
+        return as_server(&address);
     } else {
         println!("looking for someone challenge");
-        return as_client("localhost");
+        return as_client(&address);
     }
 }
 
-fn as_server() -> std::io::Result<()> {
-    let listener = TcpListener::bind(format!("localhost:{}", PORT))?;
+fn as_server(address: &str) -> std::io::Result<()> {
+    let listener = TcpListener::bind(address)?;
+
+    fn handle_client(stream: TcpStream) -> std::io::Result<()> {
+        println!("|- challenger found!\n");
+
+        GameInstance::new(
+            Board::new(),
+            Move::X,
+            |t| send_our_play(t, &stream),
+            |_| recieve_their_play(Move::O, &stream),
+        )
+        .run();
+
+        Ok(())
+    }
 
     for stream in listener.incoming() {
         handle_client(stream?)?;
     }
-
-    Ok(())
-}
-
-fn handle_client(stream: TcpStream) -> std::io::Result<()> {
-    println!("|- challenger found!\n");
-
-    GameInstance::new(
-        Board::new(),
-        Move::X,
-        |t| send_our_play(t, &stream),
-        |_| recieve_their_play(Move::O, &stream),
-    ).run();
 
     Ok(())
 }
@@ -57,8 +77,8 @@ fn send_our_play((row, col): (usize, usize), mut destination: &TcpStream) {
     }
 }
 
-fn as_client(server_ip: &str) -> std::io::Result<()> {
-    let stream = TcpStream::connect(format!("{}:{}", server_ip, PORT))?;
+fn as_client(address: &str) -> std::io::Result<()> {
+    let stream = TcpStream::connect(address)?;
     let mut board = Board::new();
 
     println!("|- challenger found!\n");
@@ -66,7 +86,7 @@ fn as_client(server_ip: &str) -> std::io::Result<()> {
 
     match recieve_their_play(Move::X, &stream) {
         None => println!("their play was invalid..."),
-        Some(Play(m, tr, tc)) => board.set_at(m, (tr, tc))
+        Some(Play(m, tr, tc)) => board.set_at(m, (tr, tc)),
     };
 
     GameInstance::new(
@@ -74,16 +94,8 @@ fn as_client(server_ip: &str) -> std::io::Result<()> {
         Move::O,
         |t| send_our_play(t, &stream),
         |_| recieve_their_play(Move::O, &stream),
-    ).run();
+    )
+    .run();
 
     Ok(())
-}
-
-fn choose_be_server() -> bool {
-    let args: Vec<String> = args().collect();
-    let choice: bool = args
-        .get(1)
-        .map(|a| a.parse().unwrap_or_default())
-        .unwrap_or_default();
-    choice
 }
